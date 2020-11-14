eb8115e42b9566c4d4d7e1a7244a75668d9eabfc
Emmilie Estabillo <emsestabillo@gmail.com>
2020-11-13 22:03:45 -0500

    Adjust js for quotes

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-function getHour(currentHour, currentMinutes) {
+function getTime(currentHour, currentMinutes) {
 	const period = document.querySelector(".period");
 	const time = document.querySelector(".time-now");
 
 	if (currentHour > 12) {
 		currentHour -= 12;
-		// currentHour = "0" + currentHour;
 		period.innerHTML = "pm";
-	// } else if (currentHour < 10) {
-	// 	currentHour = "0" + currentHour;
 	} else {
 		currentHour;
 		period.innerHTML = "am";//not working
@@ -18,17 +15,26 @@ function getHour(currentHour, currentMinutes) {
 	}
 
 	time.innerHTML = `${currentHour} : ${currentMinutes}`;
-
-	// hour.innerHTML = currentHour;
-	// minutes.innerHTML = currentMinutes;
 }
 
-axios
+function getQuote(quotesArray) {
+	console.log(quotesArray)
+	let index = Math.floor(Math.random() * quotesArray.length);
+console.log(index);
+	let chosenQuote = quotesArray[index];
+console.log(chosenQuote);
+
+	document.getElementById("quote").innerHTML = chosenQuote.text;
+	
+	document.querySelector(".author").innerHTML = chosenQuote.author;
+}
+
+Promise
 	.all([
 		axios.get("https://freegeoip.app/json/"), //city and country under time
 		axios.get("https://worldtimeapi.org/api/ip"), //time based on ip address and info on expanded state
-		axios.get("https://programming-quotes-api.herokuapp.com/quotes")
-	])
+		axios.get("https://type.fit/api/quotes")
+	]).catch(() => null)
 	.then(
 		axios.spread((location, time, quotes) => {
 			console.log(location.data.region_name);
@@ -40,17 +46,28 @@ axios
 			let currentMinutes = currentTime.getMinutes();
 
 
-      getHour(currentHour, currentMinutes);
+      getTime(currentHour, currentMinutes);
 
-      // console.log(time.data.abbreviation);
-			// console.log(time.data.day_of_year);
-			// console.log(time.data.day_of_week);
-			// console.log(time.data.week_number);
+      console.log(time.data.abbreviation);
+			console.log(time.data.day_of_year);
+			console.log(time.data.day_of_week);
+			console.log(time.data.week_number);
 
 			// Display quotes WORKING
-			const data = quotes.data;
-			document.getElementById("quote").innerHTML = 
-				data[Math.floor(Math.random() * data.length)].en;
+			const quotesArray = quotes.data;
+			
+			getQuote(quotesArray);
+			
+			// const chosenQuote = quote[Math.floor(Math.random() * quote.length)];
+
+			// document.getElementById("quote").innerHTML = 
+			// 	chosenQuote.en;
+
+			// 	const author = document.querySelector(".author");
+			// 	author.innerHTML = chosenQuote.author;
 		})
 	)
 	.catch((err) => console.error(err));
+
+document.querySelector("#refresh").addEventListener("click", getQuote);
+
