e135857af0d5789d13ead8b3d9a3ef760f6f4eb5
Emmilie Estabillo <emsestabillo@gmail.com>
2020-11-15 21:09:06 -0500

    JS for button details

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-	//DOM elements
-const region = document.querySelector('.region');
-const greeting = document.querySelector('.currently__greeting')
-const icon = document.querySelector('.icon')
-const period = document.querySelector(".period");
-const time = document.querySelector(".time-now");
 const body = document.body;
-const zone = document.querySelector('.country');
 
 
 function getTime(currentHour, currentMinutes, currently) {
+
+	const region = document.querySelector('.region');
+	const greeting = document.querySelector('.currently__greeting')
+	const icon = document.querySelector('.icon')
+	const period = document.querySelector(".period");
+	const time = document.querySelector(".time-now");
+
 	region.innerHTML = currently.abbreviation;
 	
 	if (currentHour >= 5 && currentHour <= 17 ) {
 		body.classList.add('day');
-	icon.src = './assets/desktop/icon-sun.svg';
-	icon.setAttribute("alt", "sun icon");
+		icon.src = './assets/desktop/icon-sun.svg';
+		icon.setAttribute("alt", "sun icon");
 	} else {
 		body.classList.add('night');
 		icon.src = './assets/desktop/icon-moon.svg';
@@ -29,7 +29,6 @@ function getTime(currentHour, currentMinutes, currently) {
 	} 
 	else {
 		greeting.innerHTML = "good evening";
-		
 	}
 
 	//Time setup
@@ -48,29 +47,37 @@ function getTime(currentHour, currentMinutes, currently) {
 	time.innerHTML = `${currentHour}:${currentMinutes}`;
 }
 
+
 function getLocation(ipLocation) {
+	const zone = document.querySelector('.country');
+
 	const regionName = ipLocation.region_name;
 	const countryCode = ipLocation.country_code;
 	zone.innerHTML = `in ${regionName}, ${countryCode}`;
 }
 
-function getQuote(quotesArray) {
-	quotesArray;
-	let index = Math.floor(Math.random() * quotesArray.length);
-console.log(index);
-	let chosenQuote = quotesArray[index];
-console.log(chosenQuote);
 
-	document.getElementById("quote").innerHTML = chosenQuote.text;
-	
+function getQuote(quotesArray) {
+	let index = Math.floor(Math.random() * quotesArray.length);
+	let chosenQuote = quotesArray[index];
+
+	document.getElementById("quote").textContent = chosenQuote.text;
 	document.querySelector(".author").innerHTML = chosenQuote.author;
 }
 
+
 function getDetails(currently) {
-	console.log(currently.day_of_year);
-	console.log(currently.day_of_week);
-	console.log(currently.week_number);
+	const timeZone = document.getElementById('timezone');
+	const dayOfYear = document.getElementById('year-day');
+	const weekDay = document.getElementById('week-day');
+	const weekNumber = document.getElementById('week-number');
+
+	timeZone.innerHTML = currently.timezone;
+	dayOfYear.innerHTML = currently.day_of_year;
+	weekDay.innerHTML = currently.day_of_week;
+	weekNumber.innerHTML = currently.week_number;
 }
+
 
 Promise
 	.all([
@@ -85,20 +92,41 @@ Promise
 			getLocation(ipLocation);
 
 			//Time now
-			const currently = time.data;
-			const currentTime = new Date(time.data.datetime);
+			let currently = time.data;
+			let currentTime = new Date(time.data.datetime);
 			let currentHour = currentTime.getHours();
 			let currentMinutes = currentTime.getMinutes();
+
       getTime(currentHour, currentMinutes, currently);
 
 			getDetails(currently);
 
 			// Display quotes
-			const quotesArray = quotes.data;	
+			let quotesArray = quotes.data;
+
 			getQuote(quotesArray);
 			
+			document.getElementById('refresh').addEventListener('click', getQuote)
 		})
 	)
 	.catch((err) => console.error(err));
 
+
+
+const expand = document.querySelector('.expand');
+
+function showDetails() {
+	body.classList.toggle('transform');
+
+	if (expand.firstChild.nodeValue === "More") {
+		expand.firstChild.nodeValue = "Less"
+	} else {
+		expand.firstChild.nodeValue = "More"
+	}
+
+	const arrow = document.querySelector('.arrow');
+	arrow.classList.toggle('rotate');
+}
+
 // document.getElementById('refresh').addEventListener('click', getQuote);
+expand.addEventListener('click', showDetails);
