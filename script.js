7b8d7188dd2112e64184b9f27339f2d6242cacda
Emmilie Estabillo <emsestabillo@gmail.com>
2020-11-20 22:17:24 -0500

    Add js for updating time

--- script.js
+++ script.js
@@ -1,62 +1,89 @@
+//DOM elements
 const body = document.body;
 const main = document.querySelector('.widgets');
+const region = document.querySelector('.region');
+const icon = document.querySelector('.icon')
+const time = document.querySelector(".time-now");
+const greeting = document.querySelector('.currently__greeting')
+const period = document.querySelector(".period");
+const details = document.querySelector('.details');
 
-function getTime(currentHour, currentMinutes, currently) {
-	const region = document.querySelector('.region');
-	const greeting = document.querySelector('.currently__greeting')
-	const icon = document.querySelector('.icon')
-	const period = document.querySelector(".period");
-	const time = document.querySelector(".time-now");
-	const details = document.querySelector('.details');
 
+function getRegion(currently) {
 	region.innerHTML = currently.abbreviation;
-	
-	if (currentHour >= 5 && currentHour <= 17 ) {
-		body.classList.add('day');
-		icon.src = './assets/desktop/icon-sun.svg';
-		icon.setAttribute("alt", "sun icon");
-	} else {
-		body.classList.add('night');
-		icon.src = './assets/desktop/icon-moon.svg';
-		icon.setAttribute("alt", "moon icon");
-		details.style.color = '#fff';
-		details.style.background = 'rgba(0, 0, 0, 0.75)';
+}
+
+function getTime() {
+	let currentTime = new Date();
+	let hour = currentTime.getHours()
+	let minute = currentTime.getMinutes();
+
+	//Minute format
+	if(minute < 10){
+		minute = "0" + minute
 	}
 
-	//Greeting and icon
-	let greet = '';
-
-	if (currentHour >= 5 && currentHour <= 11) {
-		greet = 'morning';
-	} else if (currentHour >= 12 && currentHour <= 17) {
-		greet = 'afternoon';
-	} 
-	else {
-		greet = 'evening';
-	}
+	//Time of day
+	// let greet = '';
+	// if (hour >= 5 && hour <= 11) {
+	// 	greet = 'morning';
+	// } else if (hour >= 12 && hour <= 17) {
+	// 	greet = 'afternoon';
+	// } 
+	// else {
+	// 	greet = 'evening';
+	// }
+	// greeting.appendChild(document.createTextNode(greet));
 	
-	greeting.appendChild(document.createTextNode(greet));
-
-	//Time setup
-	if (currentHour > 12) {
-		currentHour -= 12;
+	// Time setup
+	if (hour > 12) {
+		hour -= 12;
 		period.textContent = "pm";
 	} else {
-		currentHour;
 		period.textContent = "am";
 	}
-
-	if (currentMinutes < 10) {
-		currentMinutes = "0" + currentMinutes;
-	}
-
-	time.textContent = `${currentHour}:${currentMinutes}`;
+	time.textContent = `${hour}:${minute}`;
+	let interval = (60 - (new Date()).getSeconds()) * 1000 + 5;
+	setTimeout(getTime,interval)
 }
 
 
+
+// function getTime(currentHour, currentMinutes) {
+// 	if (currentHour >= 5 && currentHour <= 17 ) {
+// 		body.classList.add('day');
+// 		icon.src = './assets/desktop/icon-sun.svg';
+// 		icon.setAttribute("alt", "sun icon");
+// 	} else {
+// 		body.classList.add('night');
+// 		icon.src = './assets/desktop/icon-moon.svg';
+// 		icon.setAttribute("alt", "moon icon");
+// 		details.style.color = '#fff';
+// 		details.style.background = 'rgba(0, 0, 0, 0.75)';
+// 	}
+// }
+
+
+
+// 	function getPeriodOfDay(currentHour) {
+	
+// //Greeting and icon
+// 	let greet = '';
+// 	if (currentHour >= 5 && currentHour <= 11) {
+// 		greet = 'morning';
+// 	} else if (currentHour >= 12 && currentHour <= 17) {
+// 		greet = 'afternoon';
+// 	} 
+// 	else {
+// 		greet = 'evening';
+// 	}
+// 	greeting.appendChild(document.createTextNode(greet));
+
+	
+// 	}
+
 function getLocation(ipLocation) {
 	const zone = document.querySelector('.country');
-
 	const regionName = ipLocation.region_name;
 	const countryCode = ipLocation.country_code;
 	zone.innerHTML = `in ${regionName}, ${countryCode}`;
@@ -87,38 +114,47 @@ function getDetails(currently) {
 
 Promise
 	.all([
-		axios.get("https://freegeoip.app/json/"), //city and country under time
-		axios.get("https://worldtimeapi.org/api/ip"), //time based on ip address and info on expanded state
-		axios.get("https://type.fit/api/quotes")
+		axios.get("https://type.fit/api/quotes"),
+		// axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://worldtimeapi.org/api/ip`)
+		axios.get('https://worldtimeapi.org/api/ip'), //time based on ip address and info on expanded state
+		axios.get("https://freegeoip.app/json/") //city and country under time
+		
 	]).catch(() => null)
 	.then(
-		axios.spread((location, time, quotes) => {
-			//Region
-			const ipLocation = location.data;
-			getLocation(ipLocation);
+		axios.spread((quotes, time, location) => {
+			// Display quotes
+			let quotesArray = quotes.data;
+			getQuote(quotesArray);
+
+			//Get random quote
+			document.getElementById('refresh').addEventListener('click', function(){
+				axios.get('https://type.fit/api/quotes').then((quotesArray) => {
+					getQuote(quotesArray.data)
+				}).catch(function (error) {
+    			console.log(error);
+		})
+	})
 
 			//Time now
 			let currently = time.data;
+			// console.log(currently);
 			let currentTime = new Date(time.data.datetime);
 			let currentHour = currentTime.getHours();
 			let currentMinutes = currentTime.getMinutes();
-
-      getTime(currentHour, currentMinutes, currently);
-
+			let currentSeconds = currentTime.getSeconds();
+			getTime();
+			getRegion(currently)
 			getDetails(currently);
 
-			// Display quotes
-			let quotesArray = quotes.data;
-
-			getQuote(quotesArray);
-			
-			document.getElementById('refresh').addEventListener('click', getQuote)
+			//Region
+			const ipLocation = location.data;
+			getLocation(ipLocation);
 		})
 	)
 	.catch((err) => console.error(err));
 
 
-
+//Expand button
 const expand = document.querySelector('.expand');
 
 function showDetails() {
@@ -134,5 +170,11 @@ function showDetails() {
 	arrow.classList.toggle('rotate');
 }
 
-// document.getElementById('refresh').addEventListener('click', getQuote);
 expand.addEventListener('click', showDetails);
+
+// function init()
+// {
+//     var interval = (60 - (new Date()).getSeconds()) * 1000 + 5;
+//     getTime();
+//     setTimeout(init,interval);
+// }
